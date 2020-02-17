71861fe087356024d43d4351a5733838f486c95e
root <kirankumar.alluvada@citrix.com>
2020-02-17 17:03:43 +0530

    v2

--- pkg/foo_handler.go
+++ pkg/foo_handler.go
@@ -7,5 +7,5 @@ import (
 
 func Foo_handler (w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(200)
-	fmt.Fprintf(w, "\nThis is UPGRADED version of Kiran's foo package\n")
+	fmt.Fprintf(w, "\n ====  v2 =====  This is UPGRADED version of Kiran's foo package\n")
 }
